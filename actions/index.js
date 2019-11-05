a67745602d177b4d32e2d580b523561c56f43f90
Hyeeun <hyeeuni.mok@gmail.com>
2019-11-04 22:37:10 -0500

    fetching error message

--- actions/index.js
+++ actions/index.js
@@ -34,7 +34,8 @@ export const getMovies = () => {
 
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(MOVIE_DATA)
+      // resolve(MOVIE_DATA)
+      reject('Cannot fetch data!')
       }, 2000)
     })
 }
--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,8 @@ class Home extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      movies: []
+      movies: [],
+      errorMessage: ''
     }
   }
   // state  = {
@@ -81,13 +82,17 @@ class Home extends React.Component {
   // }
 
   componentDidMount() {
-    getMovies().then((movies) => {
-      this.setState({movies})
-    })
+    getMovies()
+      .then((movies) => {
+        this.setState({movies})
+     })
+      .catch((error) =>{
+        this.setState({errorMessage: error})
+      })
   }
-  
+
   render() {
-    const { movies } = this.state
+    const { movies, errorMessage } = this.state
     return (
       <div>
         <Head>
@@ -109,6 +114,11 @@ class Home extends React.Component {
               <div className="col-lg-9">
                 <Carousel />
                 <div className="row">
+                  { errorMessage && 
+                    <div className="alert alert-danger" role="alert">
+                      { errorMessage }
+                    </div>
+                  }
                   <MovieList movies={movies} />
                 </div>
               </div>
