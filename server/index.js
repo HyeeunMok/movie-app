4ad82a12e5b3ada3a138bc8f1be88a4af222fa4d
Hyeeun <hyeeuni.mok@gmail.com>
2019-11-20 07:41:07 -0500

    setting up server

---
+++ server/index.js
@@ -0,0 +1,58 @@
+
+const next = require('next')
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+
+  const server = express();
+  server.use(bodyParser.json())
+
+  server.get('/api/v1/movies', (req, res) => {
+    return res.json({message: 'Hello World'})
+  })
+
+  server.post('/api/v1/movies', (req, res) => {
+    const movie = req.body
+    console.log(JSON.stringify(movie))
+    return res.json({...movie, createdTime: 'today', author: 'Hyeeun'})
+  })
+
+  server.patch('/api/v1/movies/:id', (req, res) => {
+    const { id } = req.params
+    return res.json({message: `Updating post of id: ${id}`})
+  })
+
+  server.delete('/api/v1/movies/:id', (req, res) => {
+    const { id } = req.params
+    return res.json({message: `Deleting post of id: ${id}`})
+  })
+
+  // server.get('/faq', (req, res) => {
+  //   res.send(`
+  //     <html>
+  //       <head></head>
+  //         <body>
+  //           <h1>Hello World!</h1>
+  //         </body>
+  //     </html>
+  //   `)
+  // })
+  // handling all of the request coming to the server
+  server.get('*', (req, res) => {
+    // next.js is handling requests and providing pages where we are navigating to
+    return handle(req, res)
+    // return res.json({message: 'Hello Friends, welcome to my website!'})
+  })
+
+  const PORT = process.env.PORT || 3000;
+
+  server.listen(PORT, (err) => {
+    if (err) throw err
+    console.log('> Ready on port ' + PORT)
+  })
+})
